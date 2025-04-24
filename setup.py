b057716ba227f9c236f171f7653f8932dbf99678
bogdnvch <maksim.bogdanovich@wiseweb.by>
2025-04-24 11:55:30 +0300

    feat: change setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,18 @@ setup(
     url="https://github.com/bogdnvch/talknet_asd",
     packages=find_packages(),
     install_requires=[
+        "python-speech-features>=0.6,<0.7",
+        "scipy>=1.15.2,<2.0.0",
+        "opencv-python>=4.11.0.86,<5.0.0.0",
+        "scenedetect>=0.6.6,<0.7.0",
+        "pandas>=2.2.3,<3.0.0",
+        "gdown>=5.2.0,<6.0.0",
+        "tqdm>=4.67.1,<5.0.0",
+        "numpy>=2.2.5,<3.0.0",
+        "ffmpeg>=1.4,<2.0",
+        "torch>=2.6.0,<3.0.0",
+        "torchaudio>=2.6.0,<3.0.0",
+        "torchvision>=0.21.0,<0.22.0",
     ],
     python_requires='>=3.12',
 )
