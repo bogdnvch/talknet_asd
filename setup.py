f9412a08adf016f4443c39ced75b89f5a23d6ce1
Aleksey Korshuk <ale-kor02@mail.ru>
2025-04-26 21:48:25 +0300

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="talknet_asd",
-    version="0.1.9",
+    version="0.2.0",
     description="Active Speaker Detection using TalkNet",
     author="Maksim Bogdanovich",
     url="https://github.com/bogdnvch/talknet_asd",
