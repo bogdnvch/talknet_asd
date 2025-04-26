2aef7c2324f0853bf8823df368a98159cf0663a8
bogdnvch <maksim.bogdanovich@wiseweb.by>
2025-04-26 17:40:51 +0300

    change numpy -v

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="talknet_asd",
-    version="0.1.5",
+    version="0.1.6",
     description="Active Speaker Detection using TalkNet",
     author="Maksim Bogdanovich",
     url="https://github.com/bogdnvch/talknet_asd",
